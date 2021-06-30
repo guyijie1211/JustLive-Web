d139540ff6cfd3aaf6e3c83c14eb6c282b075226
guyijie1211 <guyijie1211@gmail.com>
2021-06-30 10:09:20 +0800

    解决浏览器缓存导致页面版本更新不生效问题，取消对虎牙弹幕lib.js的格式验证

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack")
 const CompressionWebpackPlugin = require('compression-webpack-plugin')
 const productionGzipExtensions = ['js', 'css']
+const Timestamp = new Date().getTime();
 
 module.exports = {
     configureWebpack: {
@@ -21,7 +22,11 @@ module.exports = {
                 maxChunks: 5,
                 minChunkSize: 100
             })
-        ]
+        ],
+        output: { // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
+            filename: `[name].${Timestamp}.js`,
+            chunkFilename: `[name].${Timestamp}.js`
+        },
     },
     // publicPath: './',
     devServer: {
